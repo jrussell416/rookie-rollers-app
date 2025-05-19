d9caa75737060b6fa2aa22f7afff7cda4e806696
Jennifer Russell <jc.russell@yahoo.com>
2025-05-19 19:36:33 -0400

    Add user seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,33 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+User.create!([{ # create Skater user
+                first_name: "Trixie",
+                last_name: "Franklin",
+                phone_number: "647-123-5878",
+                email: "skater@tord.com",
+                insurance: "WFTDiCanada1234",
+                role: 0,
+                password: "password",
+              },
+              { # create Trainer user
+                first_name: "Lucille",
+                last_name: "Anderson",
+                phone_number: "876-555-8065",
+                email: "trainer@tord.com",
+                insurance: "WFTDiCanada8753",
+                role: 1,
+                password: "password1",
+              },
+              { # create Admin user
+                first_name: "Julienne",
+                last_name: "Nonnatus",
+                phone_number: "020-555-8065",
+                email: "admin@tord.com",
+                insurance: "WFTDiCanada7041",
+                role: 2,
+                password: "password2",
+              }])
+
+p "Created #{User.count} users"
